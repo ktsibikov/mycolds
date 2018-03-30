8d53b2487ab6d19e432492a1bf67b2ec7ce11a49
ktsibikov <kirill.tsibikov@ideanomix.com>
2018-03-30 21:29:24 +0300

    implemented script for data set processing

---
+++ my_colds/data/handle_image_data.py
@@ -0,0 +1,55 @@
+"""
+This module will help you work with images.
+Useful tools to load, convert and process images to any
+different formats.
+"""
+
+import cv2
+import numpy as np
+
+
+class ImageProcessor:
+
+    def __init__(self, shape: tuple):
+        assert shape
+        self.h = shape[0]
+        self.w = shape[1]
+        self.d = shape[2]
+
+    def resize(self, img):
+        if img.shape[0] / self.h >= img.shape[1] / self.w:
+            img = cv2.resize(
+                img,
+                (int(self.h * img.shape[1] / img.shape[0]), self.h)
+            )
+        else:
+            img = cv2.resize(
+                img,
+                (self.w, int(self.w * img.shape[0] / img.shape[1]))
+            )
+        return img
+
+    def fil_blank(self, img_rgb):
+        if img_rgb.shape[0] == self.h:
+            int_resize_1 = img_rgb.shape[1]
+            int_fill_1 = (self.w - int_resize_1) // 2
+            int_fill_2 = self.w - int_resize_1 - int_fill_1
+            numpy_fill_1 = np.zeros((self.h, int_fill_1, 3), dtype=np.uint8)
+            numpy_fill_2 = np.zeros((self.h, int_fill_2, 3), dtype=np.uint8)
+            img_filled_rgb = np.concatenate((numpy_fill_1, img_rgb, numpy_fill_2), axis=1)
+        elif img_rgb.shape[1] == self.w:
+            int_resize_0 = img_rgb.shape[0]
+            int_fill_1 = (self.h - int_resize_0) // 2
+            int_fill_2 = self.h - int_resize_0 - int_fill_1
+            numpy_fill_1 = np.zeros((int_fill_1, self.w, 3), dtype=np.uint8)
+            numpy_fill_2 = np.zeros((int_fill_2, self.w, 3), dtype=np.uint8)
+            img_filled_rgb = np.concatenate((numpy_fill_1, img_rgb, numpy_fill_2), axis=0)
+        else:
+            raise ValueError
+        return img_filled_rgb
+
+    def __call__(self, img):
+        img = self.resize(img)
+        img = self.fil_blank(img)
+
+        return img
---
+++ my_colds/handle_dataset.py
@@ -0,0 +1,60 @@
+"""
+The script take all images from passed directory,
+process them and store final images to the passed
+output directory.
+"""
+import sys
+import os
+import glob
+import argparse
+
+import cv2
+
+from my_colds.data.handle_image_data import ImageProcessor
+
+
+def get_class_lable(file_name: str) -> str:
+    return file_name[5]
+
+
+def main(input_dir: str, output_dir: str, img_shape: tuple) -> int:
+    train_files_pattern = f'{input_dir}/*'
+    globbed = glob.glob(train_files_pattern)
+
+    assert len(globbed) > 0, f'--input-dir {input_dir} shouldn\'t be empty'
+
+    processed = []
+    process = ImageProcessor(img_shape)
+
+    for path in globbed:
+        file_name = path.rpartition('/')[2]
+        label = get_class_lable(file_name)
+        img = cv2.imread(path, cv2.COLOR_BGR2RGB)
+        images = [process(img)]
+        processed.extend(zip([label] * len(images), images))
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    for idx, (label, img) in enumerate(processed, 1):
+        path = f'{output_dir}/{label}_{idx}.jpg'
+        cv2.imwrite(path, img)
+
+    return 0
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('Process raw image dataset and store converted data.')
+    parser.add_argument('--input-dir', action='store', dest='input_dir', required=True)
+    parser.add_argument('--output-dir', action='store', dest='output_dir', required=True)
+    parser.add_argument('--final-img-dims', type=int, nargs='+', dest='img_dims', required=True)
+    args = parser.parse_args()
+    input_dir = args.input_dir
+    output_dir = args.output_dir
+    img_dims = tuple(args.img_dims)
+
+    assert len(img_dims) == 3, f'--final-img-dims {img_dims} should have 3 dimensions'
+
+    if not os.path.isdir(input_dir):
+        raise EnvironmentError(f'--input-dir {input_dir} should exist')
+
+    sys.exit(main(input_dir, output_dir, img_dims))
