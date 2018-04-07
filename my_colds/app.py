ce9ffd48abac9de36d344e16e7aef492ebda7fec
ktsibikov <kirill.tsibikov@ideanomix.com>
2018-04-07 12:41:05 +0300

    implemented stub

--- my_colds/app.py
+++ my_colds/app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+import json
+from flask import (
+    Flask,
+    request,
+    abort,
+)
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'Hi there!'
+    photo_url = request.args.get('photo_url')
+    if not photo_url:
+        return abort(400)
+    return json.dumps(
+        {
+            'is_plaque': False
+        }
+    )
 
 
 if __name__ == '__main__':
