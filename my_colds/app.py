cfda7d07922645674cf48d7f1f4321bc024ecaba
ktsibikov <kirill.tsibikov@ideanomix.com>
2018-04-14 23:14:43 +0300

    added detection plaque with CNN

--- my_colds/app.py
+++ my_colds/app.py
@@ -5,17 +5,30 @@ from flask import (
     abort,
 )
 
+from my_colds.detection.sore_throat.sore_throat_detections import PlaqueDetection
+
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     photo_url = request.args.get('photo_url')
+    res = None
     if not photo_url:
         return abort(400)
+    try:
+        detector = PlaqueDetection(photo_url)
+        not_plaque, plaque = detector.detect()
+        if plaque > not_plaque:
+            res = True
+        else:
+            res = False
+    except Exception as e:
+        abort(500, 'Something went wrong...')
+
     return json.dumps(
         {
-            'is_plaque': False
+            'is_plaque': res
         }
     )
 
